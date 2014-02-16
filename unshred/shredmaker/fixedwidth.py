a4179e7217e5b9a476a86e6a104f17a9262ff786
kien <kien.luu@base79.com>
2014-02-16 14:40:12 +0000

    Pass shred maker tests

--- unshred/shredmaker/fixedwidth.py
+++ unshred/shredmaker/fixedwidth.py
@@ -1,3 +1,4 @@
+from unshred.shred.base import WIDTH_KEY, HEIGHT_KEY
 from unshred.shredmaker.base import AbstractShredMaker
 
 
@@ -11,4 +12,10 @@ class FixedWidthVerticalShredMaker(AbstractShredMaker):
         self.force_perfect_fit = force_perfect_fit
 
     def get_shreds(self, image, shred_width):
-        return []
+        if self.force_perfect_fit and image.size[WIDTH_KEY] % shred_width != 0:
+            raise SmallerWidthException
+
+        # img.crop(0,0,1,1) returns a 1 pixel image of the first pixel at 0,0
+        return [image.crop(
+                (x, 0, x + shred_width, image.size[HEIGHT_KEY]))
+                for x in xrange(0, image.size[WIDTH_KEY], shred_width)]
